5a49dd80f93313670f6be7c21aca0157503e0829
NadavMozeson <nadavson02@gmail.com>
2024-04-07 20:30:06 +0300

    Added tree direction

--- Builder/GraphCreator.py
+++ Builder/GraphCreator.py
@@ -230,7 +230,7 @@ class Graph:
         return False
 
     def display_tree_of_graph(self):
-        tree = Graph()
+        tree = Graph(directed=True)
         for node in self.nodes:
             tree.add_node(node.label)
         for node in self.nodes:
@@ -243,8 +243,7 @@ class Graph:
             for adj_node, weight in node.adjacent_nodes.items():
                 graph.add_edge(node.label, adj_node.label)
         pos = nx.circular_layout(graph)
-        nx.draw(graph, pos=pos, node_color='lightgreen', with_labels=True, node_size=1000, width=3.0, font_size=20)
-
+        nx.draw(graph, pos=pos, node_color='lightgreen', with_labels=True, node_size=1000, width=3.0, arrowsize=25, font_size=20)
         plt.show()
 
 
